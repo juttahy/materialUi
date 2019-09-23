e24a08f8ffadaf434ab6aba6813405740e8b9ecd
Hyrskylahti Jutta <s1900548@edu.bc.fi>
2019-09-23 11:07:15 +0300

    first commit to material ui example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import { Toolbar } from '@material-ui/core';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6">
+            Material UI Example
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Button color="primary">Button</Button>
     </div>
   );
 }
